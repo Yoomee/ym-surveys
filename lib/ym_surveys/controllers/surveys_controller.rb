b6be47b2af6f999b57d9d0fa04fc067d98653d2a
Greg Woodcock <greg@yoomee.com>
2014-04-29 11:04:23 +0100

    Only allow admin users see surveys for now

--- lib/ym_surveys/controllers/surveys_controller.rb
+++ lib/ym_surveys/controllers/surveys_controller.rb
@@ -1,10 +1,14 @@
 module YmSurveys::SurveysController
 
   def show
-    @survey = Survey.find(params[:id])
-    @submission ||= @survey.submissions.build(:user => current_user)
-    @submission.build_responses
-    @question_group = SurveyQuestionGroup.find @submission.current_step
+    if current_user && current_user.admin?
+      @survey = Survey.find(params[:id])
+      @submission ||= @survey.submissions.build(:user => current_user)
+      @submission.build_responses
+      @question_group = SurveyQuestionGroup.find @submission.current_step
+    else
+      redirect_to root_path
+    end
   end
 
   def index
