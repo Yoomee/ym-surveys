44ccfeaaadb1515b6d50e7960b67899a73362682
Greg Woodcock <greg@yoomee.com>
2014-04-29 14:49:37 +0100

    Reject empty checkbox results

--- lib/ym_surveys/models/survey_question_response.rb
+++ lib/ym_surveys/models/survey_question_response.rb
@@ -6,5 +6,12 @@ module YmSurveys::SurveyQuestionResponse
     base.send(:serialize, :response)
     base.validates :response, :presence => true, :if => :required?
     base.delegate :required?, :to => :survey_question
+    base.send(:before_save, :reject_empty_array_items)
   end
+
+  private
+  def reject_empty_array_items
+    response.reject!(&:empty?) if response.is_a?(Array)
+  end
+
 end
